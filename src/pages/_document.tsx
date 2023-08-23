1f0556d1c3c3b0334e13231299f193372fe4b46c
Yuefii <muffidahmadiawan31@gmail.com>
2023-08-23 21:07:08 +0700

    title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Yuefii | Portfolio</title>
       <body className='bg-slate-100'>
         <Main />
         <NextScript />
