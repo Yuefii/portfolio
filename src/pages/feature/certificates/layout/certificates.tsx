d9843c127be4a863225518b1668b6160b38c0a9c
Yuefii <muffidahmadiawan31@gmail.com>
2023-09-19 15:49:44 +0700

    refactor(refactoring production code)

--- src/pages/feature/certificates/layout/certificates.tsx
+++ src/pages/feature/certificates/layout/certificates.tsx
@@ -7,23 +7,20 @@ const CertificateLayout = () => {
     return (
         <main>
             <div className="flex justify-center mx-auto">
-                <div className="grid gap-4 py-10 lg:w-1/3">
+                <div className="py-10 lg:w-1/3">
                     <h1 className="text-center text-2xl text-slate-900 font-bold">CERTIFICATE</h1>
                     <p className='text-slate-700 text-center mx-5'>This is a certificate that I got while learning coding in courses and bootcamps.</p>
                 </div>
             </div>
-            <h2 className="text-center text-red-700 text-bold py-5">Dicoding</h2>
+            <h2 className="text-center text-lg text-red-700 text-bold py-5">Dicoding</h2>
             <div className="flex justify-center mx-auto lg:w-1/3">
-                <div className="grid grid-cols-3 gap-2 mx-5">
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 mx-5">
                     {jsonDicoding.map((data, index) => (
                         <Link href={data.dicoding} key={index}>
                             <Image src={data.dicoding} alt="h-auto max-w-full rounded-lg" width={500} height={500} />
                         </Link>
                     ))}
                 </div>
-            </div>
-            <div className="flex justify-center mx-auto">
-                <hr />
             </div>
             <h2 className="text-center text-red-700 text-lg mt-5 text-bold py-5">Codepolitan</h2>
             <div className="flex justify-center mx-auto lg:w-1/3">
