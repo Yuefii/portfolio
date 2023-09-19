942d4187cb73e9708cd37d4b50634004f59192a1
Yuefii <muffidahmadiawan31@gmail.com>
2023-09-19 16:21:17 +0700

    delete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,15 @@ import type { AppProps } from 'next/app'
 import AppShell from './components/navbar/appShell'
 import ProfileShell from './components/profile/profileShell'
 import FooterShell from './components/footer/footerShell'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <AppShell>
-        <ProfileShell>
-          <Component {...pageProps} />
-          <FooterShell>
-          </FooterShell>
-        </ProfileShell>
-      </AppShell >
-      <Analytics />
-    </>
+    <AppShell>
+      <ProfileShell>
+        <Component {...pageProps} />
+        <FooterShell>
+        </FooterShell>
+      </ProfileShell>
+    </AppShell >
   )
 }
