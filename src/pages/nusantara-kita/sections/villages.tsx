211d33b352e694e456f080fc7ade3a65ebf93b8e
yuefii <muffidahmadiawan31@gmail.com>
2024-05-21 15:59:33 +0700

    refactor nusantara-kita

--- src/pages/nusantara-kita/sections/villages.tsx
+++ src/pages/nusantara-kita/sections/villages.tsx
@@ -12,8 +12,8 @@ const Villages = ({ show, onClick, data, setShow, value, onChange }: any) => {
         value={value || ""}
         onChange={(e) => onChange(e.target.value)}
       />
-      <div className="bg-[#161616] p-3 h-auto rounded-md flex justify-between">
-        <p className="text-blue-400">
+      <div className="bg-[#161616] p-3 h-auto rounded-md flex flex-col md:flex-row md:items-center md:justify-between">
+        <p className="text-xs md:text-base text-blue-400">
           {`${baseUrl}/api/nusantara/${
             value ? value : "{district_code}"
           }/villages`}
@@ -21,14 +21,14 @@ const Villages = ({ show, onClick, data, setShow, value, onChange }: any) => {
         {!show ? (
           <button
             onClick={onClick}
-            className="text-white text-xs py-1.5 px-3 rounded-md bg-green-600"
+            className="text-white text-xs py-1.5 px-3 mt-3 md:mt-0 rounded-md bg-green-600"
           >
             GET
           </button>
         ) : (
           <button
             onClick={() => setShow(false)}
-            className="text-white text-xs py-1.5 px-3 rounded-md bg-red-600"
+            className="text-white text-xs py-1.5 px-3 mt-3 md:mt-0 rounded-md bg-red-600"
           >
             Close
           </button>
@@ -39,7 +39,7 @@ const Villages = ({ show, onClick, data, setShow, value, onChange }: any) => {
           <p className="text-white">Response :</p>
           <div className="bg-[#161616] h-[400px] overflow-auto rounded-md">
             <ul className="text-white p-3">
-              <pre className="text-white">
+              <pre className="text-xs md:text-base text-white">
                 {JSON.stringify({ data }, null, 2)}
               </pre>
             </ul>
