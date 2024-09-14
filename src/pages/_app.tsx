0316c516a2242d0d70369d3b4e20a78327701d13
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-14 12:57:08 +0700

    chore(SEO): adding vercel analytics & speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <main>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </main>
   )
 }
