8a4ca87366153ef7602d3a202b3cf88cf94095e4
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-15 21:16:24 +0700

    chore(SEO): adding tag keyword seo

--- src/libs/SEO.tsx
+++ src/libs/SEO.tsx
@@ -4,12 +4,20 @@ import React, { useEffect, useState } from 'react'
 interface SEOProps {
   title: string
   description: string
+  keywords?: string[]
   image?: string
   url?: string
   breadcrumbs?: { name: string; url: string }[]
 }
 
-const SEO = ({ title, description, image, url, breadcrumbs }: SEOProps) => {
+const SEO = ({
+  title,
+  description,
+  keywords,
+  image,
+  url,
+  breadcrumbs
+}: SEOProps) => {
   const [fullUrl, setFullUrl] = useState('')
 
   useEffect(() => {
@@ -18,10 +26,29 @@ const SEO = ({ title, description, image, url, breadcrumbs }: SEOProps) => {
     }
   }, [fullUrl, url])
 
+  const Keywords = [
+    'portofolio',
+    'yuefii',
+    'mupid',
+    'mupid ahmadiawan',
+    'muhamad mupid ahmadiawan',
+    'pengembangan web',
+    'freelancer',
+    'portfolio online',
+    'pengembangan front-end',
+    'pengembangan back-end',
+    'pembuatan website',
+    'backend developer',
+    'pengembangan backend',
+    'developer API',
+    'freelance backend developer',
+    'backend solutions for web applications'
+  ]
+
   return (
     <Head>
       <title>{title}</title>
-      <link rel="icon" href="/logo.png" />
+      <link rel="icon" href="/favicon.ico" />
       <meta
         name="google-site-verification"
         content="Pwtm5TnyHpwMl-78sNygN-IFLcTHXpBXTmMYsmjq1Ds"
@@ -30,6 +57,10 @@ const SEO = ({ title, description, image, url, breadcrumbs }: SEOProps) => {
       <meta name="robots" content="index, follow" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta
+        name="keywords"
+        content={keywords ? keywords.join(', ') : Keywords.join(', ')}
+      />
       {image && <meta property="og:image" content={image} />}
       {fullUrl && <meta property="og:url" content={fullUrl} />}
       <meta property="og:type" content="website" />
