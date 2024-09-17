626c336ce526da61c40f81f2ec0a34d78f227684
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-18 00:23:39 +0700

    chore: setup prisma client

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,9 @@
+import prisma from '@/libs/prisma'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || '',
