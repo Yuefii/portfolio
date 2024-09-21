0b4e5366c097bb6d2eff6d3d4a6781f43b15093f
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-21 21:55:14 +0700

    chore: adding animation loading

--- src/components/homepage/SpotifyPlaylist.tsx
+++ src/components/homepage/SpotifyPlaylist.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from '../Loading'
 
 const SpotifyPlaylist = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const urlEmbed =
+    'https://open.spotify.com/embed/playlist/6czvxaZxj3cvUpV6QUbQFf?si=65d8164468964229&nd=1&dlsi=129b2f49d71f4799'
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <div className="my-5">
         <h1 className="text-2xl font-bold mb-4">Chill Vibes for Coding 🎧</h1>
-        <iframe
-          src="https://open.spotify.com/embed/playlist/6czvxaZxj3cvUpV6QUbQFf?si=65d8164468964229&nd=1&dlsi=129b2f49d71f4799"
-          width="100%"
-          height="380"
-          allow="encrypted-media"
-          className="rounded-lg"
-        ></iframe>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <iframe
+            src={urlEmbed}
+            width="100%"
+            height="380"
+            allow="encrypted-media"
+            className="rounded-lg"
+          ></iframe>
+        )}
       </div>
     </>
   )
