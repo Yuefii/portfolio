2643fe722e2e943ec7362a0c3822a9a6474ba2a8
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-30 19:25:50 +0700

    chore: restyling dark mode on chatbot

--- src/components/chatbot/BotMessage.tsx
+++ src/components/chatbot/BotMessage.tsx
@@ -5,12 +5,14 @@ import { RiRobot3Line } from 'react-icons/ri'
 const BotMessage = ({ role, content }: Message) => {
   return (
     <div className="flex w-full my-2">
-      <div className="flex justify-center items-center p-1 w-8 h-8 border bg-neutral-100 rounded-full mr-2">
-        <RiRobot3Line size={18} />
+      <div className="flex justify-center items-center p-1 w-8 h-8 bg-neutral-100 dark:bg-neutral-700 rounded-full mr-2">
+        <RiRobot3Line size={18} className="text-neutral-800 dark:text-white" />
       </div>
       <div>
-        <div className="text-neutral-800">{role}</div>
-        <p className="border p-2 rounded-md text-neutral-700">{content}</p>
+        <div className="text-neutral-800 dark:text-white">{role}</div>
+        <p className="mt-2 border py-2 px-3 rounded-2xl text-neutral-700 dark:text-neutral-300">
+          {content}
+        </p>
       </div>
     </div>
   )
