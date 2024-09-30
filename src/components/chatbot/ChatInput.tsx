78e892c8fdd370b62d11431a7a94b1bfe2d0c028
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-30 18:29:03 +0700

    chore: adding style dark mode on chatbot

--- src/components/chatbot/ChatInput.tsx
+++ src/components/chatbot/ChatInput.tsx
@@ -7,9 +7,9 @@ const ChatInput = () => {
         <input
           type="text"
           placeholder="Type your message here"
-          className="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 text-sm text-neutral-800"
+          className="flex h-10 w-full rounded-md border px-3 text-sm text-neutral-800 dark:text-white dark:bg-neutral-800"
         />
-        <button className="p-2 bg-sky-600  inline-flex items-center justify-center rounded-md text-sm text-white font-medium px-4 py-2">
+        <button className="p-2 bg-sky-600 dark:bg-rose-600 inline-flex items-center justify-center rounded-md text-sm text-white font-medium px-4 py-2">
           Send
         </button>
       </form>
