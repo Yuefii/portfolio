4da595add0e1831a26d8d57702327bd1b72120dd
Yuefii <muffidahmadiawan31@gmail.com>
2024-10-01 19:55:29 +0700

    chore: protect dashboard if user has not logged in

--- src/layouts/admin.tsx
+++ src/layouts/admin.tsx
@@ -1,11 +1,18 @@
-import Sidebar from '@/components/admin/Sidebar'
 import TopBar from '@/components/admin/TopBar'
+import Sidebar from '@/components/admin/Sidebar'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import React, { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
 }
 const AdminLayout = ({ children }: Props) => {
+  const { status } = useSession()
+  const router = useRouter()
+  if (status === 'unauthenticated') {
+    router.push(`/auth/login?redirect=${encodeURIComponent('/admin')}`)
+  }
   return (
     <div className="flex gap-4 p-4">
       <Sidebar />
