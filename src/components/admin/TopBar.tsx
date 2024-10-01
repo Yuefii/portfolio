c1219ef4faac464f585e53b34bf3e54c45c9380b
Yuefii <muffidahmadiawan31@gmail.com>
2024-10-01 19:58:05 +0700

    chore: adding gretting on dashboard

--- src/components/admin/TopBar.tsx
+++ src/components/admin/TopBar.tsx
@@ -4,12 +4,22 @@ import React from 'react'
 
 const TopBar = () => {
   const { data } = useSession()
+  const getGreeting = () => {
+    const hours = new Date().getHours()
+    if (hours < 12) {
+      return 'Good morning'
+    } else if (hours < 18) {
+      return 'Good afternoon'
+    } else {
+      return 'Good evening'
+    }
+  }
   return (
     <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
       <div className="flex items-center justify-between p-0.5">
         <div>
           <span className="text-sm font-bold block">
-            🚀 Good morning, {data?.user?.name}!
+            🚀 {getGreeting()}, {data?.user?.name}!
           </span>
           <span className="text-xs block text-stone-500">
             {formatDate(new Date())}
