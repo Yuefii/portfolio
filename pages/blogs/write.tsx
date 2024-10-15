35f7db843b6285a6565e6cfc61d710e76cab293f
Yuefii <muffidahmadiawan31@gmail.com>
2024-10-15 23:34:42 +0700

    chore(SEO): adding metadata on write pages

--- pages/blogs/write.tsx
+++ pages/blogs/write.tsx
@@ -1,8 +1,15 @@
+import SEO from '@/components/SEO'
 import Write from '@/modules/blogs/write'
 
 const Pages = () => {
   return (
     <>
+      <SEO
+        title="Write Article Blogs | Yuefii"
+        description="Create your stories and creative ideas"
+        image="https://www.yuefii.site/logo.png"
+        url="https://www.yuefii.site/blogs/write"
+      />
       <Write />
     </>
   )
