3e2e610fccb7fdb12ac5b27f3ef94cd3eb79e5ce
Yuefii <muffidahmadiawan31@gmail.com>
2024-10-26 07:25:44 +0700

    chore: fix wrong delete components

---
+++ modules/blogs/components/ButtonUploadImage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FiUpload } from 'react-icons/fi'
+
+interface ButtonUploadProps {
+  onUpload: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const ButtonUploadImage = ({ onUpload }: ButtonUploadProps) => {
+  return (
+    <label className="flex gap-x-1 items-center text-white text-sm bg-sky-600 dark:bg-rose-600 rounded-md py-1.5 px-3 cursor-pointer">
+      <FiUpload size="16" />
+      Upload Image & Publish
+      <input
+        type="file"
+        accept="image/*"
+        onChange={onUpload}
+        className="hidden"
+      />
+    </label>
+  )
+}
+
+export default ButtonUploadImage
