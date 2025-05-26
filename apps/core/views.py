6448f1faba2c09153c7a05dab272a019e68e17e8
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-26 23:32:53 +0700

    chore: refact code & menambahkan metadata seo

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'index.html')
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        seo_data = {
+            'title': 'Yuefii - Muhamad Mupid Ahmadiawan',
+            'description': 'Website pribadi untuk berbagi ilmu, pengalaman, dan cerita menarik seputar coding, pemrograman, dan teknologi.',
+            'keywords': "django, python, yuefii, muhamad mupid ahmadiawan, mupid, personal website, portfolio, backend engineer, backend developer, software engineer",
+            'og_type': "website",
+            'og_image': "https://yuefii.site/static/img/profile.png",
+            'twitter_card': "summary_large_image",
+        }
+
+        about_data = {
+            'username': "Yuefii",
+        }
+
+        context['seo'] = seo_data
+        context['about'] = about_data
+        context['request'] = self.request
+
+        return context
