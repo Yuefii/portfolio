8d503d63398d4bb9b6bdfdeb127696b3d87dd69d
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-27 00:26:23 +0700

    chore: register halaman about pada apss

--- api/settings.py
+++ api/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.core'
+    'apps.core',
+    'apps.about',
 ]
 
 MIDDLEWARE = [
--- api/urls.py
+++ api/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.core.urls')),
+    path('about/', include('apps.about.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
