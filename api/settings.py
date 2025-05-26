100d381655bb4011334ad916dcc75d55195706c5
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-26 21:52:22 +0700

    chore: merubah setting.py

--- api/settings.py
+++ api/settings.py
@@ -111,8 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
