65bb74fefcea63306c1e10242d611c3bc8587665
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-26 22:09:26 +0700

    chore: konfigurasi host

--- api/settings.py
+++ api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'www.yuefii.site', 'yuefii.site']
 
 
 # Application definition
